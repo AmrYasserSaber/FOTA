0bc6e25240ef37b3f7ff744fbcbbd1a0cfb2745c
amrya <amr.yassr.2@gmail.com>
2024-02-20 17:02:31 +0200

    initial

---
+++ API/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from API.routes import programme
+
+app: FastAPI = FastAPI()
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(programme.programmeRouter)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True, host='localhost', port=8000)
